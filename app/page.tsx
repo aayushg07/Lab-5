a705f2cf78fd261982d1de8cf3dea903043367fb
aayushg07 <aayushgautam.ag@gmail.com>
2025-02-07 18:06:34 -0500

    Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import styles from "@/app/ui/home.module.css";
 import { lusitana } from "@/app/ui/fonts";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -29,24 +29,24 @@ export default function Page() {
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-            
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-        src="/hero-desktop.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="Screenshots of the dashboard project showing desktop version"
-      />
-      <Image
-      src="/hero-mobile.png"
-      width={560}
-      height={620}
-      className="block md:hidden"
-      alt="Screenshots of the dashboard project showing mobile version"/>
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
